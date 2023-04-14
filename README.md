3a34febe898f703b1ca9fb26a242dd90ede52ffd
Victor Ruiz <vicruiz85@gmail.com>
2023-04-14 16:37:14 +0200

    Initialized again git-101-boilerplate

---
+++ README.md
@@ -0,0 +1,2 @@
+# git-101-boilerplate
+boilerplate for setup lecture DS
---
+++ hello_batch.py
@@ -0,0 +1,7 @@
+def helloBatch(number=None):
+    return f'\nWelcome batch #{number}\n'
+
+
+if __name__ == '__main__':
+    mynumber = None
+    print(helloBatch(mynumber))
